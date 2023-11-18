f3aabd25b4a0d7d8eac60cba3028ccfc6d0463d1
clesav <dolclem@yahoo.fr>
2023-11-18 11:10:44 +0000

    Header corrections

--- tests/test_core_firmware.py
+++ tests/test_core_firmware.py
@@ -1,3 +1,22 @@
+# test_core_firmware.py
+#
+# Copyright 2023 Clement Savergne <csavergne@yahoo.com>
+#
+# This file is part of yasim-avr.
+#
+# yasim-avr is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# yasim-avr is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with yasim-avr.  If not, see <http://www.gnu.org/licenses/>.
+
 import pytest
 import os
 import yasimavr.lib.core as corelib
