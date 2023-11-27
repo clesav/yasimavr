61e13143903bfa76201095b0035151405fb15f33
clesav <dolclem@yahoo.fr>
2023-11-27 18:34:12 +0000

    Test update

--- tests/test_core_firmware.py
+++ tests/test_core_firmware.py
@@ -47,8 +47,8 @@ def test_flash(firmware):
     assert len(blocks) >= 2 #we should have at least .text, .data
 
     #Read the .data block and check the content
-    stdata_block = [ b for b in blocks if len(b.mem_block) == 6][0]
-    stdata = bytes(stdata_block.mem_block)
+    stdata_block = [ b for b in blocks if len(b.buf) == 6][0]
+    stdata = bytes(stdata_block.buf)
     #The string is "Char" +2 null chars (1 for string termination +1 for word alignement)
     assert stdata == b'Char\0\0'
 
@@ -59,7 +59,7 @@ def test_eeprom(firmware):
 
     b = blocks[0]
     assert b.base == 0
-    assert bytes(b.mem_block) == b'EEPROM\0'
+    assert bytes(b.buf) == b'EEPROM\0'
 
 
 def test_fusesm(firmware):
@@ -68,7 +68,7 @@ def test_fusesm(firmware):
 
     b = blocks[0]
     assert b.base == 0
-    assert bytes(b.mem_block) == bytes([0x12, 0x34, 0x56])
+    assert bytes(b.buf) == bytes([0x12, 0x34, 0x56])
 
 
 def test_memory_load(firmware):
@@ -76,7 +76,7 @@ def test_memory_load(firmware):
     firmware.load_memory(Area.Flash, nvm)
 
     blocks = firmware.blocks(Area.Flash)
-    data_block = [ b for b in blocks if len(b.mem_block) == 6][0]
+    data_block = [ b for b in blocks if len(b.buf) == 6][0]
 
-    m = nvm.block(data_block.base, len(data_block.mem_block))
-    assert bytes(m) == bytes(data_block.mem_block)
+    m = nvm.block(data_block.base, len(data_block.buf))
+    assert bytes(m) == bytes(data_block.buf)
